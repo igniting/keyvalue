356141f1c1ea3b76e0e412d9720fc71a7df9d82a
Anshu Avinash <anshu.avinash35@gmail.com>
2014-12-19 01:26:03 +0530

    Added a simple key value store

---
+++ KeyValue.hs
@@ -0,0 +1,43 @@
+-- Simple key value store
+
+import System.Directory
+import qualified Data.HashMap.Strict as HM
+import Control.Monad
+
+type RecordMap = HM.HashMap String String
+
+-- Name of the file containing records
+fileName = "records.dat"
+
+-- Delimiter between key value
+delim = ' '
+
+createFileIfNotExists :: FilePath -> IO ()
+createFileIfNotExists name = do
+    check <- doesFileExist name
+    unless check $ writeFile name ""
+
+getKeyValue :: String -> (String, String)
+getKeyValue s = (x, y) where
+  x = fst p
+  y = (tail . snd) p
+  p = span (/= delim) s
+
+-- Initialize the database
+initDB :: IO RecordMap
+initDB = do
+    -- Create the file if not exists
+    createFileIfNotExists fileName
+    allcontent <- readFile fileName
+    return (HM.fromList (map getKeyValue (lines allcontent)))
+
+deserialize :: String -> String -> String
+deserialize k v = k ++ [delim] ++ v ++ "\n"
+
+-- Insert a key value in the database
+put :: String -> String -> IO ()
+put k v = appendFile fileName (deserialize k v)
+
+-- Get the value of the key
+get :: RecordMap -> String -> String
+get m k = HM.lookupDefault "" k m
