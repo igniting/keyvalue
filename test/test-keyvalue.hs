14123ad3a627f8ce8c0d180f702318242e163f0c
Anshu Avinash <anshu.avinash35@gmail.com>
2015-02-20 21:35:09 +0530

    Add test for mergeDataLogs

--- test/test-keyvalue.hs
+++ test/test-keyvalue.hs
@@ -1,29 +1,51 @@
 import           Control.Concurrent
-import           Data.ByteString.Char8
+import           Data.ByteString.Char8   (pack, unpack)
+import           Data.List
 import qualified Database.KeyValue       as KV
 import           System.Directory
+import           System.IO.Temp
 import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
 
-baseDir :: String
-baseDir = "/tmp/keyvalue"
+initialize :: FilePath -> IO (MVar KV.KeyValue)
+initialize dir = KV.initDB cfg where
+  cfg = KV.Config dir
 
-initialize :: IO (MVar KV.KeyValue)
-initialize = KV.initDB cfg where
-  cfg = KV.Config baseDir
-
-cleanup :: MVar KV.KeyValue -> IO ()
-cleanup m = do
+cleanup :: FilePath -> MVar KV.KeyValue -> IO ()
+cleanup dir m = do
   KV.closeDB m
-  removeDirectoryRecursive baseDir
+  removeDirectoryRecursive dir
 
-testDb :: String -> Property
-testDb s = monadicIO $ do
-  m <- run initialize
+testGet :: MVar KV.KeyValue -> String -> Property
+testGet m s = monadicIO $ do
   run $ KV.put m (pack s) (pack s)
   v <- run $ KV.get m (pack s)
   assert (v == Just (pack s))
-  run $ cleanup m
+
+testMerge :: [String] -> Property
+testMerge xs = monadicIO $ do
+  dir <- run $ createTempDirectory "/tmp" "keyvalue."
+  run $ mapM_ (insertKey dir) xs
+  run $ KV.mergeDataLogs dir
+  m <- run $ initialize dir
+  keys <- run $ fmap (map unpack) (KV.listKeys m)
+  assert (keys `isEquiv` xs)
+  run $ cleanup dir m
+
+isEquiv :: [String] -> [String] -> Bool
+isEquiv xs ys = (nub . sort) xs == (nub . sort) ys
+
+insertKey :: FilePath -> String -> IO ()
+insertKey dir s = do
+  m <- initialize dir
+  KV.put m (pack s) (pack s)
+  KV.closeDB m
 
 main :: IO ()
-main = quickCheck testDb
+main = do
+  dir <- createTempDirectory "/tmp" "keyvalue."
+  m <- initialize dir
+  quickCheck (testGet m)
+  cleanup dir m
+  quickCheck testMerge
+  return ()
