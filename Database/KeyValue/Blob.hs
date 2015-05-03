9b57e4cbd1915b04de26ef1b19bb34b6bdab1a4e
Anshu Avinash <anshu.avinash35@gmail.com>
2015-05-03 16:15:30 +0530

    Re-export Data.Blob module

---
+++ Database/KeyValue/Blob.hs
@@ -0,0 +1,9 @@
+{-
+  Module      : Database.KeyValue.Blob
+  Description : Contains methods for handling blob data type
+  Stability   : Experimental
+-}
+
+module Database.KeyValue.Blob (module Data.Blob) where
+
+import           Data.Blob
